de5c72b4bf4d51fc98dd56cf0b99c536ef1b4653
Wellington Ferreira <141442241+WoodyWell@users.noreply.github.com>
2023-08-14 16:17:12 -0300

    Add files via upload

---
+++ ex074.py
@@ -0,0 +1,5 @@
+from random import randint
+num = randint(0,9), randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9)
+print(f'Os valores sorteados foram: {num}')
+print(f'O maior valor sorteado foi: {max(num)}')
+print(f'O menor valor sorteado foi: {min(num)}')
