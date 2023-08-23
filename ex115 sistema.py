724ee5623a88f37de8a44cd373aa8d9d8a46cb9f
Wellington Ferreira <141442241+WoodyWell@users.noreply.github.com>
2023-08-23 13:06:35 -0300

    Add files via upload

---
+++ ex115 sistema.py
@@ -0,0 +1,25 @@
+from ex115.lib.interface import *
+from ex115.lib.arquivo import *
+from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
+
+while True:
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do sistema'])
+    if resposta == 1:
+        lerArquivo(arq)
+    elif resposta == 2:
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = numint('Idade: ')
+        cadastrar(arq, nome, idade)
+    elif resposta == 3:
+        cabeçalho('Saindo do sistema... Até logo!')
+        break
+    else:
+        print(f'\033[31mErro! Digite uma opção válida!\033[m')
+    sleep(1.5)
